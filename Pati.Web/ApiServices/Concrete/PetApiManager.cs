f5f8a8b67153a56d849773d23e61473d487f7a4b
SercanBayrambey <sercanbayrambeyy@gmail.com>
2021-01-06 19:17:15 +0300

    ADDED: List method to PetApiService

--- Pati.Web/ApiServices/Concrete/PetApiManager.cs
+++ Pati.Web/ApiServices/Concrete/PetApiManager.cs
@@ -1,12 +1,44 @@
-﻿using Pati.Web.ApiServices.Interfaces;
+﻿using Microsoft.AspNetCore.WebUtilities;
+using Newtonsoft.Json;
+using Pati.Web.ApiServices.Interfaces;
+using Pati.Web.Dtos;
+using Pati.Web.Results;
+using Pati.Web.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Pati.Web.ApiServices.Concrete
 {
     public class PetApiManager : IPetApiService
     {
+        private readonly HttpClient _httpClient;
+
+        public PetApiManager(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri(StaticVars.BaseAPIAdress);
+        }
+
+        public async Task<IDataResult<List<PetDto>>> List(int currentPage = 1)
+        {
+            var query = new Dictionary<string, string>
+            {
+                ["pageId"] = currentPage.ToString()
+            };
+
+            var response = await _httpClient.GetAsync(QueryHelpers.AddQueryString("getPets", query));
+            if (response.IsSuccessStatusCode)
+            {
+                var dto = JsonConvert.DeserializeObject<List<PetDto>>(await response.Content.ReadAsStringAsync());
+                return new DataResult<List<PetDto>>(dto, true, response.StatusCode);
+            }
+            else
+            {
+                return new DataResult<List<PetDto>>(null, false, response.StatusCode);
+            }
+        }
     }
 }
--- Pati.Web/Areas/Member/Controllers/PetsController.cs
+++ Pati.Web/Areas/Member/Controllers/PetsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Pati.Web.ApiServices.Interfaces;
 using Pati.Web.CustomFilters;
 using Pati.Web.Dtos;
 using Pati.Web.StringConsts;
@@ -11,13 +12,22 @@ namespace Pati.Web.Areas.Member.Controllers
 {
     public class PetsController : BaseController
     {
-        public IActionResult Index()
+        private readonly IPetApiService _petApiService;
+        public PetsController(IPetApiService petApiService)
         {
-            List<PetDto> pets = new List<PetDto>();
-
-            pets.Add(new PetDto { PetName = "Reçel", PetBirthDate = DateTime.Now, PetAdditionInfo = "Çok güzel bir hayvan", Gender = "Erkek", Genus = "Sokak Kedisi" });
-            pets.Add(new PetDto { PetName = "Çakıl", PetBirthDate = DateTime.Now, PetAdditionInfo = "Çok güzel bir hayvan", Gender = "Karı", Genus = "Muhabbet kuşu" });
-            return View(pets);
+            _petApiService = petApiService;
+        }
+        public async Task<IActionResult> Index(int currentPage = 1)
+        {
+            var request = await _petApiService.List(currentPage);
+            if (request.Success)
+            {
+                return View(request.Data);
+            }
+            else
+            {
+                return View(new List<PetDto>());
+            }
         }
 
         public IActionResult Details(int id)
